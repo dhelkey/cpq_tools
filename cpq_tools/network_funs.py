540dd02c0eb30f01984965fc036089a999576981
dhelkey <dhelkey@stanford.edu>
2023-11-16 08:22:14 -0800

    add network functions

---
+++ cpq_tools/network_funs.py
@@ -0,0 +1,9 @@
+#Functions for working with network data
+#NOTE - this will be deprecated and moved to its own package shortly
+
+import warnings
+
+warnings.warn(
+    "Network functions be deprecated and moved to a new package in future versions.",
+    FutureWarning
+)
