2e26bf8a427ab153af0db36b3a73a505e9338e04
Daniel Helkey <dhelkey@stanford.edu>
2024-02-07 17:34:07 -0800

    STATE_data add variables to initialize

--- STATE_DATA_process.py
+++ STATE_DATA_process.py
@@ -2,9 +2,10 @@
 Process STATE_DATA, save overall STATE_DATA and individual [STATE]:
                     INFANT and LONG records
                         For the FULL dataset and subsampled QUICK dataset
-                        
+
 NOTE -The last part of this script (combining datasets)
-         requires storing all datasets in MEMORY, and may cause RAM errors
+         requires storing all datasets in MEMORY, and may cause RAM errors without sufficient memory.
+         Larger dataserts may require keeping states seperate.
     
 
 OUTPUTS (PICKLE outputs - Stored in PHI data directory)
@@ -40,18 +41,17 @@ def state_data_process_infant(df_in):
         df[var_with_na] = df[var_without_na]
         df.loc[df[var_without_na].isin(na_code_vec), 
                va_with_na] = np.nan
+        
+    return df
 
     #Construct date variables
 
     #Identify infant survival
 
 #Full data storage
-        
-# Creating the data_type_vec and data_length_vec as specified
 data_type_vec = ['infant', 'long']
 data_length_vec = ['full', 'quick']
 
- # Simplified dictionary initialization with comprehension
 full_data_dict = {data_type: {data_length: [] for \
                 data_length in data_length_vec} for \
                      data_type in data_type_vec}
@@ -61,16 +61,17 @@ for state, (file_infant, file_long) in STATE_DATA_FILES.items():
     file_dict = {'infant':file_infant, 'long':file_long}
     for data_type, file_use in file_dict.items():
         if data_type=='long':
-            continue #Finalize INFANT processing code first
+            continue #Finalize INFANT processing code before reading in LONG code
         file_path = setup.phi_data_path + file_use
 
         df_raw = read_csv_stata(file_path)
         df_raw['state'] = state
 
-        df_processsed, df_subsampled = setup.process_state_data(df_raw)
-
+        df_processsed = setup.process_state_data(df_raw)
+        df_subsampled = None #Add subsampling here
+        
         #Print rows, columns of df_raw vs df_processed
-
+        print('raw', df_raw.shape,'processed', df_processed.shape)
         #Print number of subsampled rows
 
         quick_dict = {'full':df_processsed,
@@ -88,85 +89,7 @@ for state, (file_infant, file_long) in STATE_DATA_FILES.items():
 for data_type in data_type_vec:
             for data_length in data_length_vec:
                 full_save_file_name = f"STATE_DATA_{data_type}_{data_length}.pkl"
-                full_save_file_path = setup.phi_data_path
+                full_save_file_path = os.path.join(setup.phi_data_path,
+                                                   full_save_file_name)
                 full_out_df = pd.concat(data_list[data_type][data_length])
-                full_out_df.to_pickle(full_out_df)
-
-#Save full STATE_DATA datasets
-
-        
-
-
-    # file_clean = PHI_DATA_PATH + file_clean
-    # file_long = PHI_DATA_PATH + file_long
-# dat_clean_list = []
-# dat_long_list = []
-
-# out_file = PHI_DATA_PATH + 'STATE_DATA.pkl'
-
-    # # Read the clean file
-    # df_clean = pd.read_csv(file_clean) if file_clean.endswith('.csv') \
-    #     else pd.read_stata(file_clean)
-    # df_clean['state'] = state
-    # df_clean.columns = df_clean.columns.str.lower()
-    # dat_clean_list.append(df_clean)
-
-    # # Read the long file
-    # df_long = pd.read_csv(file_long) if file_long.endswith('.csv')\
-    #         else pd.read_stata(file_long)
-    # df_long['state'] = state
-    # df_long.columns = df_long.columns.str.lower()
-    # dat_long_list.append(df_long)
-# 
-    #Subsample datasets for quick analysis
-
-    #Write out INFANT and LONG datasets for each state
-
-# Concatenate the lists into DataFrames
-# dat_clean = pd.concat(dat_clean_list)
-# dat_long = pd.concat(dat_long_list)
-# if quick:
-#     dat_clean = dat_clean.sample(n=10000)
-
-
-#
-
-# dat_clean.to_pickle(out_file)
-
-# dat_clean.state.value_counts()
-
-
-
-#Import & process data by state
-
-# INFANT & LONG datasets (First, process INFANT records, then process LONG records)
-#Merge infant level records into the LONG dataset
-#The LONG dataset will be used for network analysis
-
-
-#Save each as seperate PKL (Python object) files
-
-
-
-
-#Variable preperation & cleaning
-
-
-
-
-#Prepare a 
-
-
-
-
-
-
-#Prepare a subsampled version of the data
-
-
-
-
-
-
-
-#
+                full_out_df.to_pickle(full_save_file_path)
