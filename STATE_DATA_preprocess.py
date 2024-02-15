d88ceb1e345b4edaf4f27df0feb5748aeeca10f0
Daniel Helkey <dhelkey@stanford.edu>
2024-02-14 17:22:59 -0800

    add preprocessing structure

--- STATE_DATA_preprocess.py
+++ STATE_DATA_preprocess.py
@@ -2,7 +2,7 @@
 #constructing hospital data 
 
 
-#Import 
+#Import Charge dataframes -> convert to dictionary 
 
 
 #Reverse search - 
@@ -11,25 +11,15 @@ search = SearchEngine()
 # from uszipcode import Zipcode
 import numpy as np
 
+#Import hospital list, reverse search to identify zipcode.
 #define zipcode search function
 def get_zipcode(lat, long):
     result = search.by_coordinates(lat = lat, lng = long, returns = 1)
     print(result)
     return result[0].zipcode
 
-
-
-#
-
-
 #Test this function
-# get_zipcode(39.519042, -119.814866)
-# get_zipcode(39.519586, -119.813490)
 get_zipcode(39.619197, -112.814088)
-
-# # #load columns from dataframe
-# lat = df['Latitude']
-# lon = df['Longitude']
 
 #define latitude/longitude for function
 # df = pd.DataFrame({‘lat’:lat, ‘lon’:lon})
