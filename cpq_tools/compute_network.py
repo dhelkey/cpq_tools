196f3d2bf4ae60e485948693f00d5084b1124d23
Daniel Helkey <dhelkey@stanford.edu>
2024-02-22 12:30:05 -0800

    :memo: compute_network update

--- cpq_tools/compute_network.py
+++ cpq_tools/compute_network.py
@@ -7,6 +7,6 @@ def compute_network(df_in, from_var='prevhsp', to_var='hospid',
                         edge_cutoff=1, 
                         random_walk_steps = 5):
     """
-    Placeholder function
+    Placeholder function - awaiting LICENSE.
     """
     pass
